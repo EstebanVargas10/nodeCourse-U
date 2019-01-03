8f9940355c9a4e16993df7926d3486a3d217871b
github_username <estebanvargas10@gmail.com>
2019-01-02 20:28:02 -0600

    package.js added

--- notesNode/app.js
+++ notesNode/app.js
@@ -1,18 +1,8 @@
 console.log('Starting App');
 
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
+
+
 const notes = require('./notes.js');
-
-var user = os.userInfo();
-
-
-var res = notes.addNote();
-console.log('Results: ', notes.add(9, -8));
-
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`, function(err) {
-//     if (err){
-//         console.log('Unable to write to file :(');
-//     }
-// });
 
--- notesNode/notes.js
+++ notesNode/notes.js
@@ -1,13 +1,2 @@
 console.log('Starting Notes.js');
 
-module.exports.addNote = () => {
-    console.log('Add note')
-
-};
-
-// console.log(module);
-
-module.exports.add = (a, b) => {
-    return a+b;
-
-};
