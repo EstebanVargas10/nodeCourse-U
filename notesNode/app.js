01ee0e249d587d12db2181238cd9689dee458a21
github_username <estebanvargas10@gmail.com>
2019-01-02 19:59:37 -0600

    from vs code

--- notesNode/app.js
+++ notesNode/app.js
@@ -1,10 +1,17 @@
 console.log('Starting App');
 
 const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
 
-fs.appendFile('greetings.txt', 'Hello World', function(err) {
-    if (err){
-        console.log('Unable to write to file :(');
-    }
-});
+var user = os.userInfo();
 
+
+var res = notes.addNote();
+console.log('Results: ', notes.add(9, -8));
+
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`, function(err) {
+//     if (err){
+//         console.log('Unable to write to file :(');
+//     }
+// });
---
+++ notesNode/notes.js
@@ -0,0 +1,13 @@
+console.log('Starting Notes.js');
+
+module.exports.addNote = () => {
+    console.log('Add note')
+
+};
+
+// console.log(module);
+
+module.exports.add = (a, b) => {
+    return a+b;
+
+};
